d3a6bdfc4338d97cca7a5006faa939ff93fe0192
Je Rome <103312494+hashira09p@users.noreply.github.com>
2024-09-14 14:35:47 +0000

    Done task126

--- task126.java
+++ task126.java
@@ -1,1 +1,34 @@
+abstract class abstractClass {
+    abstract void abstractMethod();
+    
+    void concreteMethod() { 
+        System.out.println("This is a concrete method.");
+    }
+}
+
+class B extends abstractClass {
+    @Override
+    void abstractMethod() {
+        System.out.println("B's implementation of abstractMethod.");
+    }
+}
+
+class C extends abstractClass {
+    @Override
+    void abstractMethod() {
+        System.out.println("C's implementation of abstractMethod.");
+    }
+}
+
+public class task126 {
+    public static void main(String[] args) {
+        B b = new B();
+        b.abstractMethod();  // Outputs: B's implementation of abstractMethod.
+        b.concreteMethod();  // Outputs: This is a concrete method.
+        
+        C c = new C();
+        c.abstractMethod();  // Outputs: C's implementation of abstractMethod.
+        c.concreteMethod();  // Outputs: This is a concrete method.
+    }
+}
 
